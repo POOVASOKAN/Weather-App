694147f7f24254cf2a74bfceefb55bf6b42056f0
poovasokan <poovasokan@gmail.com>
2023-12-08 16:42:15 +0000

    updated

---
+++ script.js
@@ -0,0 +1,48 @@
+const apiKey = "23ee8b598138f8ed04f9af17e18ae459";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    document.querySelector(".weather").style.display = "block";
+
+    var data = await response.json();
+
+    //! Displaying in console
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) / 10 + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
+
+    //! Updating the images based on the weather condition
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "assests/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "assests/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "assests/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "assests/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "assests/mist.png";
+    }
+
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
